40ac08f7dc6d0967b5047eda29e71128240daf71
Christofher Ontiveros Espino <78229432+VileDruidGG@users.noreply.github.com>
2025-10-01 10:48:12 -0600

    Initial Commit

---
+++ EquiPayTests/EquiPayTests.swift
@@ -0,0 +1,17 @@
+//
+//  EquiPayTests.swift
+//  EquiPayTests
+//
+//  Created by Christofher Ontiveros Espino on 01/10/25.
+//
+
+import Testing
+@testable import EquiPay
+
+struct EquiPayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
